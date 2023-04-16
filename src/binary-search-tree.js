ebfc0324dddf6917235372a662143c48130f0cd3
XeniyaMV <mehnuchka.xd@gmail.com>
2023-04-16 14:17:35 -0700

    feat: resolved binary search tree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,43 +7,212 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor() {
+    this.treeRoot = '';
+  }
+  
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.treeRoot === '') {
+      return null;
+    }
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add( data ) {
+    if (this.has(data)) {
+      return;
+    }
+    
+    let node = {
+      'data': data,
+      'parent': '',
+      'childLeft': '',
+      'childRight': ''
+    }
+    let root = this.root();
+    
+    if (!root) {
+      this.treeRoot = node;
+      return;
+
+    }
+   
+    let parent = root;
+    while(1) {
+      if (node.data > parent.data) {
+        if (parent.childRight !== '') {
+          parent = parent.childRight;
+        }
+        else {
+          node.parent = parent;
+          parent.childRight = node;
+          break;
+        }
+      }
+      if (node.data < parent.data) {
+        if (parent.childLeft !== '') {
+          parent = parent.childLeft;
+        }
+        else {
+          node.parent = parent;
+          parent.childLeft = node;
+          break;
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let parent = this.root();
+    if (!parent) {
+      return false;
+    }
+    while(1) {
+      if (data > parent.data) {
+        if (parent.childRight === '') {
+          return false;
+        }
+        parent = parent.childRight;
+
+      }
+      else if (data < parent.data) {
+        if (parent.childLeft === '') {
+          return false;
+        }
+        parent = parent.childLeft;
+      }
+      else {
+        return true;
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find( data ) {
+    if (!this.has(data)) {
+      return null;
+    }
+
+    let parent = this.root();
+    while(1) {
+      if (data > parent.data) {
+        parent = parent.childRight;
+
+      }
+      else if (data < parent.data) {
+        parent = parent.childLeft;
+      }
+      else {
+        return parent;
+      }
+    }
+    
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove( data ) {    
+    let node = this.find(data);
+    if (node === null) {
+      return;
+    }
+    let parent = node.parent;
+    let leave = node.childRight;
+    
+    if (parent === '') {
+      if (leave === '') {
+        this.treeRoot = node.childLeft;
+        this.treeRoot.parent = '';
+        return;
+      }
+      else {
+        while(1) {
+          if (leave.childLeft === '') {
+            leave.childLeft = node.childLeft;
+            if (node.childLeft !== '') {
+              leave.childLeft.parent = leave;
+            }
+            break;
+          }
+          leave = leave.childLeft;
+        }
+        this.treeRoot = node.childRight;
+        this.treeRoot.parent = '';
+        return;
+      }
+    }
+    if (leave === '') {
+      if (data > parent.data) {
+        parent.childRight = node.childLeft;
+        if (node.childLeft !== '') {
+          parent.childRight.parent = parent;
+          
+        }
+
+      }
+      if (data < parent.data) {
+        parent.childLeft = node.childLeft;
+        if (node.childLeft !== '') {
+          parent.childLeft.parent = parent;
+        }
+
+      }
+      return;     
+    }
+    
+    while(1) {
+      if (leave.childLeft === '') {
+        leave.childLeft = node.childLeft;
+        if (node.childLeft !== '') {
+          leave.childLeft.parent = leave;
+        }        break;
+      }
+      leave = leave.childLeft;
+    }
+
+    if (data > parent.data) {
+      parent.childRight = node.childRight;
+      parent.childRight.parent = parent;
+
+    }
+    else {
+      parent.childLeft = node.childRight;
+      parent.childLeft.parent = parent;
+      
+    }
+    return;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let parent = this.root();
+    if (!parent) {
+      return null;
+    }
+    while(1) {
+      if (parent.childLeft === '') {
+        return parent.data;
+      }
+      else {
+        parent = parent.childLeft;
+      }
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let parent = this.root();
+    if (!parent) {
+      return null;
+    }
+    while(1) {
+      if (parent.childRight === '') {
+        return parent.data;
+      }
+      else {
+        parent = parent.childRight;
+      }
+    }
   }
 }
 
 module.exports = {
   BinarySearchTree
 };
+
+
